8e309dee426222d7773e8021a0975b6362ee17a7
jnelbaylen <jnelbaylen@gmail.com>
2025-04-24 09:18:19 +0800

    chore: calculator

---
+++ easy/calculator.ts
@@ -0,0 +1,18 @@
+function calculate(numOne: number, numTwo: number, operation: string): number | null{
+    
+    if (operation === '+') {
+        return numOne + numTwo;
+      } else if (operation === "-") {
+        return numOne - numTwo;
+      } else if (operation === "*") {
+        return numOne * numTwo;
+      } else if (operation === "/") {
+        return numTwo !== 0 ? numOne / numTwo : null;
+      } else {
+        return null;
+      }
+
+}
+
+
+console.log(calculate(0, 5, '/'));
